881aa770ad1b1e6b379cfd265d8e16aaa57974ce
Yulia Milovanova <preachermars@gmail.com>
2020-12-21 13:35:29 +0300

    сделала пример

--- RTFImportTest/ViewController.swift
+++ RTFImportTest/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tv: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = Bundle.main.url(forResource: "test2", withExtension: "rtf")!
+        let opts : [NSAttributedString.DocumentReadingOptionKey : Any] =
+            [.documentType : NSAttributedString.DocumentType.rtf]
+        let s = try! NSAttributedString(url: url, options: opts, documentAttributes: nil)
+        self.tv.attributedText = s
     }
 
 
